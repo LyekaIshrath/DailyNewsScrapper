808577a96699117201f002141be0ad4305ad7f20
lyeka <lyekaishrath@gmail.com>
2026-06-26 23:48:32 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from email.message import EmailMessage
+
+
+url = "https://news.ycombinator.com/"
+
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    print("Website connected successfully!")
+except Exception as e:
+    print("Error connecting to website:")
+    print(e)
+    exit()
+
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+
+titles = soup.select(".titleline")
+
+headlines = []
+
+for title in titles[:10]:
+
+    headline = title.get_text(strip=True)
+
+    link = title.find("a")["href"]
+
+    # Convert relative links to full links
+    if link.startswith("item?"):
+        link = "https://news.ycombinator.com/" + link
+
+    headlines.append((headline, link))
+
+
+print("\nTop 10 Hacker News Headlines\n")
+
+for i, (headline, link) in enumerate(headlines, start=1):
+
+    print(f"{i}. {headline}")
+    print(link)
+    print("-" * 60)
+
+
+sender = "lyeka016@gmail.com"
+receiver = "lyeka016@gmail.com"
+
+app_password = "ixhx yask zvfw ngci"
+
+
+email = EmailMessage()
+
+email["Subject"] = "Daily Tech Headlines"
+
+email["From"] = sender
+
+email["To"] = receiver
+
+body = """
+Hello,
+
+Here are today's Top 10 Hacker News Headlines.
+
+"""
+
+for i, (headline, link) in enumerate(headlines, start=1):
+
+    body += f"{i}. {headline}\n"
+    body += f"{link}\n\n"
+
+body += "Regards,\nDaily News Scraper"
+
+email.set_content(body)
+
+
+try:
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+
+    server.starttls()
+
+    server.login(sender, app_password)
+
+    server.send_message(email)
+
+    server.quit()
+
+    print("\nEmail Sent Successfully!")
+
+except Exception as e:
+
+    print("\nError Sending Email")
+
+    print(e)
